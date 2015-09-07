1a1581e0bff9ab8ea51b77dbbfee8a62265a2c1e
Chen Di <sundychenya1018@yahoo.com>
2015-09-07 23:03:25 +0800

    implement HTTP class

--- WebProxy.java
+++ WebProxy.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 /**
  * Created by chendi on 3/9/15.
@@ -80,6 +83,9 @@ class ProxyThreadRunnable implements Runnable {
 
                 // Receive client request
                 BufferedInputStream buf_input = new BufferedInputStream(this.socket.getInputStream());
+                HTTP clientRequest = getClientRequest(buf_input);
+
+                /*
                 int requestLength = buf_input.read(requestBuffer);
                 String browserRequest = new String(requestBuffer, 0, requestLength);
                 System.out.println("== Client request: \n" + browserRequest);
@@ -100,6 +106,8 @@ class ProxyThreadRunnable implements Runnable {
                 System.out.println("== Return server response");
 
                 hostSocket.close();
+                */
+
                 this.socket.close();
                 System.out.println("!== End of communication");
             }
@@ -108,26 +116,145 @@ class ProxyThreadRunnable implements Runnable {
         }
     }
 
-    private void returnResponse(BufferedInputStream forwardInputStream, BufferedOutputStream clientOutputStream) throws IOException {
-        int numResponseBytes;
-        byte[] buffer = new byte[1024];
-        while ((numResponseBytes = forwardInputStream.read(buffer)) != -1) {
-            System.out.println("Receiving from server" + numResponseBytes + " bytes");
-            clientOutputStream.write(buffer, 0, numResponseBytes);
-        }
-        clientOutputStream.flush();
+    /*
+    * Read client request content into a byte buffer
+    * */
+    private HTTP getClientRequest(BufferedInputStream input) {
+
+        HTTP HTTP;
+        String header = readHeader(input);
+        ByteBuffer content_byte = readContent(input);
+
+        return new HTTP(header, content_byte);
     }
 
+    /*
+    * Read http header into String
+    * */
+    private String readHeader(BufferedInputStream input) {
+        String header = "";
+        Scanner sc = new Scanner(input);
+
+        String next;
+        while(!(next = sc.nextLine()).equals("")) {
+            header += next + "\n";
+        }
+
+        return header;
+    }
+
+    /*
+    * Read the content of the HTTP request into a byte buffer
+    * */
+    private ByteBuffer readContent(BufferedInputStream input) {
+        ByteBuffer content_byte = null;
+        byte[] input_buf = new byte[1024];
+
+        try {
+            while (input.available() > 0) {
+                input.read(input_buf);
+                content_byte.put(input_buf);
+            }
+        } catch (IOException e) {
+            System.out.println("Error read client request stream");
+            e.printStackTrace();
+        }
+
+        return content_byte;
+    }
+
+    /*
+    * Return the remote server response to the client
+    * */
+    private void returnResponse(BufferedInputStream serverResponse, BufferedOutputStream clientSocket) throws IOException {
+        int numResponseBytes;
+        byte[] buffer = new byte[1024];
+
+        while ((numResponseBytes = serverResponse.read(buffer)) != -1) {
+            System.out.println("Receiving from server" + numResponseBytes + " bytes");
+            clientSocket.write(buffer, 0, numResponseBytes);
+        }
+        clientSocket.flush();
+    }
+
+    /*
+    * Forward client request to remote server
+    * */
     private void forwardRequest(byte[] requestBuffer, int buffer, Socket hostSocket) throws IOException {
         BufferedOutputStream forwardSocket = new BufferedOutputStream(hostSocket.getOutputStream());
         forwardSocket.write(requestBuffer, 0, buffer);
         forwardSocket.flush();
     }
 
-    private String getHost(String browserRequest) {
-        int start = browserRequest.indexOf("Host: ") + 6;
-        int end = browserRequest.indexOf('\n', start);
+}
 
-        return browserRequest.substring(start, end - 1);
+
+/*
+* Client Request
+*
+* An abstraction of a client request
+* */
+class HTTP {
+    private String header;
+    private LinkedList<String> headerList;
+    private ByteBuffer byte_content;
+
+    public HTTP(String header, ByteBuffer input) {
+        this.header = header;
+        this.headerList = headerToList(header);
+        this.byte_content = input;
+    }
+
+    public String getHeader() {
+        return this.header;
+    }
+
+    public ByteBuffer getContent() {
+        return this.byte_content;
+    }
+
+    public String getHost() {
+        return retrieveHost();
+    }
+
+    public String getContentType() {
+        return retrieveContentType();
+    }
+
+    private String retrieveHost() {
+        return getField("Host:");
+    }
+
+    private String retrieveContentType() {
+        return getField("Content-Type:");
+    }
+
+    /*
+    * Retrieve a specific field from headerList
+    * */
+    private String getField(String fieldName) {
+        String field = null;
+
+        String cur_field;
+        for (int i = 0; i < this.headerList.size(); i++) {
+            cur_field = this.headerList.get(i);
+            if (cur_field.toUpperCase().startsWith(fieldName.toUpperCase())) {
+                field = (cur_field.split(":")[1]).trim();
+            }
+        }
+        return field;
+    }
+
+    /*
+    * Transform header String into LinkedList
+    * */
+    private LinkedList<String> headerToList(String header) {
+        LinkedList <String> headerList = new LinkedList<String>();
+        String[] headerArr = header.split("\n");
+
+        for (int i = 0; i < headerArr.length; i++) {
+            headerList.add(headerArr[i]);
+        }
+        return headerList;
     }
 }
